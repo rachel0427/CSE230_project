9f058cc41c313bd4d9d40d8d41af374959e2fc63
angelajc1 <42723603+angelajc1@users.noreply.github.com>
2023-11-28 03:08:16 +0000

    Add activities

---
+++ src/Activity.hs
@@ -0,0 +1,8 @@
+module Activities where
+
+import qualified Game as G
+
+actFish       = G.Activity ("Fish",           2, 5,  0, -5, 0.5)
+actPick       = G.Activity ("Pick Coconuts",  2, 5, 10, -5, 0.5)
+actDrink      = G.Activity ("Drink water",    1, 0, 10, -5, 0.5)
+actNothing    = G.Activity ("Do nothing",     1, 0,  0,  1, 0.5)
