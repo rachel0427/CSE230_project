536b4b4677a6e08307803b1c51dcccf1a39210b9
Sheep <67578212+Rachel-2000@users.noreply.github.com>
2023-11-29 18:35:29 +0000

    UI update

---
+++ src/Types.hs
@@ -0,0 +1,9 @@
+module Types (module Types) where
+import Brick      
+
+-- data PlayStatus = PlayStatus
+--                     { hunger :: Int
+--                     , health :: Int
+--                     , weather :: Weather
+--                     , date :: Int
+--                     } deriving (Show, Eq)
